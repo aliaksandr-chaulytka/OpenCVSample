b85324b1a75636998c41b73e3732ea1f32df8d9c
aliaksandr.chaulytka@gmail.com <aliaksandr.chaulytka@gmail.com>
2021-08-27 01:16:16 +0300

    Add openCV

--- app/src/main/java/dev/alex/opencvsample/MainActivity.kt
+++ app/src/main/java/dev/alex/opencvsample/MainActivity.kt
@@ -1,9 +1,20 @@
 package dev.alex.opencvsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        if (OpenCVLoader.initDebug()) {
+            Log.d("AABB", "OpenCv configured successfully")
+        } else {
+            Log.d("AABB", "OpenCv doesn’t configured successfully")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
